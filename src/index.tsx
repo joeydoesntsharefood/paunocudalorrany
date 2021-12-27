d08495ef0abe9ea3a8b3c83d14bafec57eb9cc9e
Mateus Sponton <71439733+joeydoesntsharefood@users.noreply.github.com>
2021-12-27 00:30:47 -0300

    Add files via upload

---
+++ src/index.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import reportWebVitals from './reportWebVitals';
+import StartPlay from './StartPlay/StartPlay';
+
+ReactDOM.render(<StartPlay />, document.getElementById('root'));
+
+reportWebVitals();
